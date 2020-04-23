b76236142c08af93003ca2c266103cf6507ab9ce
liushimin <liushimin@qiyi.com>
2020-04-23 23:40:48 +0800

    temp save

--- main/yushui.go
+++ main/yushui.go
@@ -2,6 +2,41 @@ package main
 
 import "fmt"
 
+func trap(height []int) int {
+
+	length := len(height)
+	var leftMax = make([]int, length)
+	var rightMax = make([]int, length)
+	lmax := height[0]
+	for i := 0; i < length-1; i++ {
+		if height[i] > lmax {
+			lmax = height[i]
+		}
+		leftMax[i+1] = lmax
+	}
+	rmax := height[length-1]
+	for i := length - 2; i > 0; i-- {
+		rightMax[i] = rmax
+		if height[i] > rmax {
+			rmax = height[i]
+		}
+	}
+	sum := 0
+	for i := 1; i < length-1; i++ {
+		if height[i] > leftMax[i] && height[i] > rightMax[i] {
+			continue
+		}
+		if leftMax[i] > rightMax[i] {
+			sum += rightMax[i]
+		} else {
+			sum += leftMax[i]
+		}
+	}
+	return sum
+}
+//[0,1,0,2,1,0,1,3,2,1,2,1]
 func main() {
-	fmt.Println("hehe")
+	a := []int{0,1,0,2,1,0,1,3,2,1,2,1}
+
+	fmt.Println(trap(a))
 }
