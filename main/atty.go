7c0b1de90d7ac80760d0f785646c325bbf075048
liushimin <liushimin@sensetime.com>
2020-04-27 18:13:44 +0800

    pack beibao

--- main/atty.go
+++ main/atty.go
@@ -1,29 +1,36 @@
 package main
 
 import "fmt"
-
+/*
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 2 2 2 2 2 3 3 3 3 3 4 4 4 4 4 5 5 5 5 5 6 6
+1 1 1 1 1 2 2 2 2 2 4 4 4 4 4 6 6 6 6 6 9 9 9 9 9 12 12
+1 1 1 1 1 2 2 2 2 2 4 4 4 4 4 6 6 6 6 6 9 9 9 9 9 13 13
+ans:13
+f(4,90)=f(3,90)+f(3,90-25)+f(3,90-2*25)+f(3,90-3*25)
+ */
 func waysToChange(n int) int {
-	if n < 0 {
-		return 0
+	var ans = make([]int, n+5)
+	coin := []int{1,5,10,25}
+	ans[0] =1
+	for i := 0; i < 4; i++ {
+		for j:=0; j<=n; j++ {
+			if j-coin[i] >= 0 {
+				ans[j] += ans[j-coin[i]]
+			}
+		}
+		for j:=0; j<=n; j++ {
+			fmt.Printf("%v ",ans[j])
+		}
+		fmt.Println()
 	}
-	if n== 0 {
-		return 1
-	}
-	if n > 25 {
-		return waysToChange(n-25)
-	}
-	if n > 10 {
-		return waysToChange(n-10)
-	}
-	if n > 5 {
-		return waysToChange(n-5)
-	}
-	if n < 5 {
-		return 1
-	}
-	return 1
+	return ans[n]
+
 }
+
+
+
 func main() {
-	ans := waysToChange(6)
+	ans := waysToChange(5)
 	fmt.Printf("ans:%v",ans)
 }
