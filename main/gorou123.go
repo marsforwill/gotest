0edb3f3b468ef197d0a06ca21006880f110ef3e2
liushimin <liushimin@sensetime.com>
2020-06-02 12:37:47 +0800

    test go current

---
+++ main/gorou123.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	// test wait group
+	// print in go routine with channel order
+	wg := sync.WaitGroup{}
+	wg.Add(10)
+	for i := 1; i <= 10; i++ {
+		ch := make(chan int)
+		go func(num int) {
+			fmt.Println(num)
+			wg.Done()
+			ch <- i
+		}(i)
+		<-ch
+	}
+	wg.Wait()
+	//time.Sleep(time.Second)
+	println("done")
+	// test for select
+	c := make(chan bool)
+	go func() {
+		time.Sleep(time.Second)
+		c <- true
+	}()
+	select {
+	case ch1 := <-c:
+		println(ch1)
+	case <-time.After(time.Second):
+		println("time out")
+
+	}
+}
