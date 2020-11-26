324558b4c0a4311ac9c74e42c859da5a0b309a4d
liushimin <liushimin@sensetime.com>
2020-11-26 17:20:38 +0800

    bfs format

--- main/bfs.go
+++ main/bfs.go
@@ -1,7 +1,69 @@
 package main
 
+import (
+	"container/list"
+	"fmt"
+	"strconv"
+)
+
+// 模版 BFS
 func slidingPuzzle(board [][]int) int {
 	m := 2
 	n := 3
 	target := "123450"
+	var start string
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			start += strconv.Itoa(board[i][j])
+		}
+	}
+	//fmt.Println(start)
+	//fmt.Println(target)
+	// 六个位置的相邻遍历 方便六个位置的0与下一步的位置交换
+	neighbor := [][]int{{1, 3},
+		{0, 4, 2},
+		{1, 5},
+		{0, 4},
+		{3, 1, 5},
+		{4, 2},
+	}
+	q := list.New()
+	// 处理过的状态存储
+	visited := make(map[string]bool)
+	// 双向list当作bfs队列
+	q.PushBack(start)
+	visited[start] = true
+
+	step := 0
+	for q.Len() > 0 {
+		sz := q.Len()
+		// bfs每一层的遍历
+		for i := 0; i < sz; i++ {
+			cur := q.Front().Value.(string)
+			q.Remove(q.Front())
+			if target == cur {
+				return step
+			}
+			idx := 0
+			for idx = 0; cur[idx] != '0'; idx++ {
+			}
+			for _, adj := range neighbor[idx] {
+				newBoard := []byte(cur)
+				// swap
+				temp := newBoard[idx]
+				newBoard[idx] = newBoard[adj]
+				newBoard[adj] = temp
+				if _, ok := visited[string(newBoard)]; !ok {
+					visited[string(newBoard)] = true
+					q.PushBack(string(newBoard))
+				}
+			}
+		}
+		step++
+	}
+	return -1
+}
+
+func main() {
+	fmt.Println(slidingPuzzle([][]int{{3, 2, 4}, {1, 5, 0}}))
 }
