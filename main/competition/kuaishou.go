e5fccf99794e675ba939f7569a4bdd9f28453ab6
liushimin <liushimin@sensetime.com>
2021-03-09 19:24:56 +0800

    chengji dp

--- main/competition/kuaishou.go
+++ main/competition/kuaishou.go
@@ -76,7 +76,45 @@ func dfsset(ans *[][]int, numq []int, temp []int, index int) {
 	dfsset(ans, numq, temp, index+1)
 }
 
+// 乘积最大连续子数组 自然的dp定义不满足最优子结构 需要根据正负来分类讨论转移
+func maxProduct(nums []int) int {
+	maxF, minF := nums[0], nums[0]
+	maxn := []int{nums[0]}
+	minn := []int{nums[0]}
+	for i := 1; i < len(nums); i++ {
+		mx, mn := maxF, minF // max for cur +，min for cur -
+		maxF = max(mx*nums[i], max(nums[i], mn*nums[i]))
+		minF = min(mn*nums[i], min(nums[i], mx*nums[i]))
+		maxn = append(maxn, maxF)
+		minn = append(minn, minF)
+	}
+	ans := nums[0]
+	for i := 0; i < len(nums); i++ {
+		if ans < maxn[i] {
+			ans = maxn[i]
+		}
+	}
+	fmt.Printf("%v\n", maxn)
+	fmt.Printf("%v\n", minn)
+	return ans
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
 func main() {
-	ans := subsets([]int{9, 0, 3, 5, 7})
-	fmt.Println(ans)
+	//ans := subsets([]int{9, 0, 3, 5, 7})
+	//fmt.Println(ans)
+	fmt.Println(maxProduct([]int{1, 0, -1, 7, 8, 0, -2}))
 }
