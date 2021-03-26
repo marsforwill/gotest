5f6a567757a97fd9d8b4d52cde3dd4b6cc7fbe42
liushimin <liushimin@sensetime.com>
2021-03-26 14:52:04 +0800

    always corner case ??? f

--- main/competition/microsoft.go
+++ main/competition/microsoft.go
@@ -88,6 +88,20 @@ func p(num int, pow int) int {
 	return ans
 }
 
+//48. 旋转图像
+func rotate(matrix [][]int)  {
+	n := len(matrix)
+	for i := 0; i < n/2; i++ {
+		for j := 0; j < (n+1)/2 ; j++ {
+			temp := matrix[i][j]
+			matrix[i][j] = matrix[n - j - 1][i];
+			matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+			matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+			matrix[j][n - i - 1] = temp
+		}
+	}
+}
+
 func main() {
 	//println(reverseWords("  hello world  "))
 	fmt.Println(numberToWords(1234567891))
